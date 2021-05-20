38445ccdbdcb3bed254f165b7c54cb92a0927308
Nasake46 <wilfrid.salles-otta@supinternet.fr>
2021-05-20 12:12:48 +0200

    installation

--- Config/dbconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-try {
-    $database = new PDO('mysql:host=127.0.0.1;dbname=mydb;charset=utf8', 'root', '');
-} catch (Exception $e) {
-    var_dump('Erreur : '.$e->getMessage());die;
-}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '64335ff92f5cc1d3a90067e1179a3f84ad1a1ba4',
+    'name' => 'wilfrid/buy-selling',
+  ),
+  'versions' => 
+  array (
+    'bramus/router' => 
+    array (
+      'pretty_version' => '1.5',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6adc0182dc6b0ebc22fd10543f65a36de10d8c05',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc3063ba22c2a1fd2f45ed856374d79114998f91',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.2.8',
+      'version' => '5.2.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd693200a73fae179d27f8f1b16b4faf3e8569eba',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v3.3.2',
+      'version' => '3.3.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '21578f00e83d4a82ecfa3d50752b609f13de6790',
+    ),
+    'wilfrid/buy-selling' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64335ff92f5cc1d3a90067e1179a3f84ad1a1ba4',
+    ),
+  ),
+);

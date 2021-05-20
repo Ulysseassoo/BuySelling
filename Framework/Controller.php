277b7f2ed264a3850a018b8bee86e7138fa205bc
Nasake46 <wilfrid.salles-otta@supinternet.fr>
2021-05-20 12:53:06 +0200

    controller

---
+++ Framework/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Framework;
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+class Controller
+{
+    private $templating;
+
+    public function __construct()
+    {
+        $loader = new FilesystemLoader('../App/View');
+        $this->templating = new Environment($loader);
+        $this->templating->addGlobal("session", $_SESSION);
+    }
+
+    protected function renderTemplate(string $templateName, array $templateParameters = [])
+    {
+        echo $this->templating->render($templateName, $templateParameters);
+    }
+}
